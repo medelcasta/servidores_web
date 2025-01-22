f6ef7fbe906ca0eaef6009775a9d912d08526531
auroramedel <amr0094@alu.medac.es>
2025-01-22 09:30:19 +0100

    ejercicios

---
+++ 07_apis/estudios/index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Estudios</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <?php 
+    error_reporting( E_ALL );
+    ini_set("display_errors", 1 );  
+    ?>
+</head>
+<body>
+    <?php 
+        $apiUrl = "http://localhost/Ejercicios/07_apis/estudios/api_estudios.php";
+        $curl = curl_init();
+        curl_setopt($curl, CURLOPT_URL, $apiUrl);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        $respuesta = curl_exec($curl);
+        curl_close($curl);
+
+        $estudios = json_decode($respuesta, true);
+        //print_r($estudios);
+    ?>
+    <div class="container">
+    <table class="table table-striped table-hover">
+        <thead class="table-dark">
+            <tr>
+                <th>Estudio</th>
+                <th>Ciudad</th>
+                <th>Año de fundacion</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php 
+                foreach($estudios as $estudio){ ?>
+                    <tr>
+                        <td><?php echo $estudio["nombre_estudio"]?></td>
+                        <td><?php echo $estudio["ciudad"]?></td>
+                        <td><?php echo $estudio["anno_fundacion"]?></td>
+                    </tr>
+                <?php } ?>
+        </tbody>
+    </table>
+    </div>
+</body>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+
+</html>
