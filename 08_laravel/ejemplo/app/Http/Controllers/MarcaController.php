7fb31dfebf8c5a7f81b5b4399b9bc8f0cc62754a
auroramedel <amr0094@alu.medac.es>
2025-02-14 14:34:15 +0100

    ejercicios

--- 08_laravel/ejemplo/app/Http/Controllers/MarcaController.php
+++ 08_laravel/ejemplo/app/Http/Controllers/MarcaController.php
@@ -29,7 +29,7 @@ class MarcaController extends Controller
      */
     public function create()
     {
-        //
+        return view('marcas/create');
     }
 
     /**
@@ -37,7 +37,13 @@ class MarcaController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $marca = new Marca;
+        $marca -> marca = $request -> input("marca");
+        $marca -> ano_fundacion = $request -> input("ano_fundacion");
+        $marca -> pais = $request -> input("pais");
+        $marca -> save();
+
+        return redirect('/marcas');
     }
 
     /**
